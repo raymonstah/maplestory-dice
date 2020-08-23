9495835e0cb857a9a95afe5122223b07c5f7aac3
Raymond Ho <rayho@ucdavis.edu>
2020-08-22 17:45:30 -0700

    start on dice roller

---
+++ script.js
@@ -0,0 +1,17 @@
+var dice = document.getElementById('dice');
+dice.style.cursor = 'pointer';
+dice.onclick = function() {
+    roll();
+};
+
+
+function roll() {
+    var str = document.getElementById('stat-str');
+    var dex = document.getElementById('stat-dex');
+    var int = document.getElementById('stat-int');
+    var luk = document.getElementById('stat-luk');
+
+    var total = 25;
+
+
+}
